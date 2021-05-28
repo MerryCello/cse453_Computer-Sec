ec2d47e63c87b08b11f70ba44252b307b71c5403
GrumpyCello <kfoniciello@gmail.com>
2021-05-28 14:01:47 -0600

    setup tests

--- injection.cpp
+++ injection.cpp
@@ -6,16 +6,17 @@ using namespace std;
 typedef string (*GenQueryFunction)(string, string);
 
 struct Credentials;
+struct TestSet;
 string genQuery(string, string);
 string genQueryWeak(string, string);
 string genQueryStrong(string, string);
 void runTestCases(char);
-//void runTests(string, GenQueryFunction, Credentials*, int, string);
-void testValid(GenQueryFunction);
-void testTautology(GenQueryFunction);
-void testUnion(GenQueryFunction);
-void testAddState(GenQueryFunction);
-void testComment(GenQueryFunction);
+void runTests(TestSet&);
+void testValid(TestSet&);
+void testTautology(TestSet&);
+void testUnion(TestSet&);
+void testAddState(TestSet&);
+void testComment(TestSet&);
 
 
 int main() {
@@ -48,10 +49,8 @@ int main() {
    return 0;
 }
 
-string genQuery(string username, string password){
-    // SELECT authenticate FROM passwordList WHERE name='$Username' and passwd='$Password'
-    string sqlQuery = "SELECT authenticate FROM passwordList WHERE name='" + username + "' and passwd='" + password + "';";
-    return sqlQuery;
+string genQuery(string username, string password) {
+    return "SELECT authenticate FROM passwordList WHERE name='" + username + "' and passwd='" + password + "';";
 }
 
 /*******************************************************************
@@ -59,8 +58,6 @@ string genQuery(string username, string password){
  *      - Strip the username and password of <, >, &, ;, "", etc. 
  *******************************************************************/
 string genQueryWeak(string username, string password) {
-    
-
    // SELECT authenticate FROM passwordList WHERE name='$Username' and passwd='$Password'
    string sqlQuery = "SELECT authenticate FROM passwordList WHERE name='" + username + "' and passwd='" + password + "';";
    string cleanQuery;
@@ -68,10 +65,12 @@ string genQueryWeak(string username, string password) {
       char invalidChars[] = {'<', '>', '&', ';', '"'};
       for (char invalidChar : invalidChars) {
          if (c == invalidChar) {
-               cleanQuery += "";
-               break;
+            cleanQuery += "";
+            break;
+         } else {
+            cleanQuery += c;
+            break;
          }
-         cleanQuery += c;
       }
    }
 
@@ -109,32 +108,65 @@ string genQueryStrong(string username, string password) {
 /*******************************************************************
  * TESTS
  *******************************************************************/
+
+struct Credentials {
+   Credentials(string username, string password) : username(std::move(username)), password(std::move(password)) {}
+
+   string username;
+   string password;
+};
+enum TestStatuses {
+   TODO = 0,
+   FAILED = 1,
+   PASSED = 2
+};
+struct TestCase {
+   Credentials input;
+   string expectedOutput;
+   string errorMessage;
+   TestStatuses status = TODO;
+};
+struct TestSet {
+   string title;
+   string name;
+   GenQueryFunction functionTested = nullptr;
+   int testsNumber = 0;
+   int testsPassedCount = 0;
+   TestCase *testCases = nullptr;
+};
+
+
 void runTestCases(char mitigationType) {
-   GenQueryFunction genQueryFunction = nullptr;
-   string caseTitle;
+   TestSet testSets[3];
+   string setGroupTitle;
    switch (mitigationType) {
       /// Regular
       case 'r': {
-         caseTitle = "Running tests on \"genQuery()\"...\n";
-         genQueryFunction = genQuery;
+         testSets[0].title = "genQuery()";
+         testSets[0].functionTested = genQuery;
          break;
       }
       /// Weak
       case 'w': {
-         caseTitle = "Running tests on \"genQueryWeak()\"...\n";
-         genQueryFunction = genQueryWeak;
+         testSets[0].title = "genQueryWeak()";
+         testSets[0].functionTested = genQueryWeak;
          break;
       }
       /// Strong
       case 's': {
-         caseTitle = "Running tests on \"genQueryStrong()\"...\n";
-         genQueryFunction = genQueryStrong;
+         testSets[0].title = "genQueryStrong()";
+         testSets[0].functionTested = genQueryStrong;
          break;
       }
       /// All
       case 'a': {
-         caseTitle = "Running tests on all genQueries...\n";
-         genQueryFunction = nullptr;
+         setGroupTitle = "RUNNING TESTS ON ALL GEN_QUERIES...";
+         testSets[0].title = "genQuery()";
+         testSets[0].functionTested = genQuery;
+         testSets[1].title = "genQueryWeak()";
+         testSets[1].functionTested = genQueryWeak;
+         testSets[2].title = "genQueryStrong()";
+         testSets[2].functionTested = genQueryStrong;
          break;
       }
 
@@ -143,243 +175,184 @@ void runTestCases(char mitigationType) {
    }
 
    /// Run all tests
-   cout << caseTitle;
-   if (!genQueryFunction) {
-      GenQueryFunction functions[] = {genQuery, genQueryWeak, genQueryStrong};
-      for (auto & function : functions) {
-         testValid(function);
-         testTautology(function);
-         testUnion(function);
-         testAddState(function);
-         testComment(function);
+   if (mitigationType == 'a') {
+      cout << setGroupTitle << endl;
+
+      int testsPassedCount = 0;
+      for (TestSet & testSet : testSets) {
+         cout << "   Running set \"" << testSet.title << "\"...\n";
+         testValid(testSet);
+         testTautology(testSet);
+         testUnion(testSet);
+         testAddState(testSet);
+         testComment(testSet);
+         testsPassedCount += testSet.testsPassedCount;
+         cout << "   **************************************************************************\n"
+              << "   *  " << testSet.testsPassedCount << "/25 tests passed for \"" << testSet.title << "\"\n"
+              << "   **************************************************************************\n\n";
       }
+      cout << "==========================================================================\n"
+           << "|| TOTAL: " << testsPassedCount << "/" << 3*25 << " tests passed\n"
+           << "==========================================================================\n\n";
    }
+   /// Run the specified test
    else {
-      testValid(genQueryFunction);
-      testTautology(genQueryFunction);
-      testUnion(genQueryFunction);
-      testAddState(genQueryFunction);
-      testComment(genQueryFunction);
+      cout << "Running set \"" << testSets[0].title << "\"...\n";
+      testValid(testSets[0]);
+      testTautology(testSets[0]);
+      testUnion(testSets[0]);
+      testAddState(testSets[0]);
+      testComment(testSets[0]);
+
+      cout << "**************************************************************************\n"
+           << "*  " << testSets[0].testsPassedCount << "/25 tests passed for \"" << testSets[0].title << "\"\n"
+           << "**************************************************************************\n\n";
    }
 }
 
-
-// function testValid() {
-//     genQuery("brian064", "secret")
-//     genQuery("roger222", "secret")
-//     genQuery("brian064", "secret")
-//     genQuery("brian064", "secret")
-//     genQuery("brian064", "secret")
-// }
-struct Credentials {
-   string username;
-   string password;
-};
-
-void testValid(GenQueryFunction genQueryFunction) {
+void testValid(TestSet& testSet) {
    /// setup
-   cout << "\nValid input:\n";
-   // todo: setup test
-   Credentials testValidCredentials[] = {
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" }
+   testSet.name = "Valid input";
+   testSet.testsNumber = 5;
+   string errorMessage = "Query not properly build with VALID input";
+   // TODO: setup tests
+   testSet.testCases = new TestCase[5] {
+      // { username, password, expected output }
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage }
    };
 
-   /// test
-   for (auto & credential : testValidCredentials) {
-      cout << "   Input (username/password):\n"
-           << "      " << credential.username << endl
-           << "      " << credential.password << endl;
-      string query = genQueryFunction(credential.username, credential.password);
+   /// test & validate
+   runTests(testSet);
 
-   /// verify
-      // todo: properly verify
-      if (query == "")
-         cout << "      PASSED: Nothing was injected\n";
-      else
-         cout << "      FAILED: Query not properly build with VALID input\n";
-   }
+   delete testSet.testCases;
+   testSet.testCases = nullptr;
 }
 
 
-void testTautology(GenQueryFunction genQueryFunction) {
+void testTautology(TestSet& testSet) {
    /// setup
-   cout << "\nTautology exploitation:\n";
-   // todo: setup test
-   Credentials testTautologyCredentials[] = {
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" }
+   testSet.name = "Tautology exploitation";
+   testSet.testsNumber = 5;
+   string errorMessage = "Query failed due to a BOOLEAN attack";
+   // TODO: setup tests
+   testSet.testCases = new TestCase[5] {
+           // { username, password, expected output }
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage }
    };
 
-   /// test
-   for (auto & credential : testTautologyCredentials) {
-      cout << "   Input (username/password):\n"
-           << "      " << credential.username << endl
-           << "      " << credential.password << endl;
-      string query = genQueryFunction(credential.username, credential.password);
+   /// test & validate
+   runTests(testSet);
 
-   /// verify
-      // todo: properly verify
-      if (query == "")
-         cout << "      PASSED: Nothing was injected\n";
-      else
-         cout << "      FAILED: Query failed due to a BOOLEAN attack\n";
-   }
+   delete testSet.testCases;
+   testSet.testCases = nullptr;
 }
 
 
-void testUnion(GenQueryFunction genQueryFunction) {
+void testUnion(TestSet& testSet) {
    /// setup
-   cout << "\nUnion exploitation:\n";
-   // todo: setup test
-   Credentials testUnionCredentials[] = {
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" }
+   testSet.name = "Union exploitation";
+   testSet.testsNumber = 5;
+   string errorMessage = "Query was altered to execute a UNION";
+   // TODO: setup tests
+   testSet.testCases = new TestCase[5] {
+           // { username, password, expected output }
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage }
    };
 
-   /// test
-   for (auto & credential : testUnionCredentials) {
-      cout << "   Input (username/password):\n"
-           << "      " << credential.username << endl
-           << "      " << credential.password << endl;
-      string query = genQueryFunction(credential.username, credential.password);
+   /// test & validate
+   runTests(testSet);
 
-   /// verify
-      // todo: properly verify
-      if (query == "")
-         cout << "      PASSED: Nothing was injected\n";
-      else
-         cout << "      FAILED: Query was altered to execute a UNION\n";
-   }
+   delete testSet.testCases;
+   testSet.testCases = nullptr;
 }
 
 
-void testAddState(GenQueryFunction genQueryFunction) {
+void testAddState(TestSet& testSet) {
    /// setup
-   cout << "\nAdd State exploitation:\n";
-   // todo: setup test
-   Credentials testAddStateCredentials[] = {
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" }
+   testSet.name = "Add State exploitation";
+   testSet.testsNumber = 5;
+   string errorMessage = "A state was added to the query";
+   // TODO: setup tests
+   testSet.testCases = new TestCase[5] {
+           // { username, password, expected output }
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage }
    };
 
-   /// test
-   for (auto & credential : testAddStateCredentials) {
-      cout << "   Input (username/password):\n"
-           << "      " << credential.username << endl
-           << "      " << credential.password << endl;
-      string query = genQueryFunction(credential.username, credential.password);
+   /// test & validate
+   runTests(testSet);
 
-   /// verify
-      // todo: properly verify
-      if (query == "")
-         cout << "      PASSED: Nothing was injected\n";
-      else
-         cout << "      FAILED: A state was added to the query\n";
-   }
+   delete testSet.testCases;
+   testSet.testCases = nullptr;
 }
 
 
-void testComment(GenQueryFunction genQueryFunction) {
+void testComment(TestSet& testSet) {
    /// setup
-   cout << "\nComment exploitation:\n";
-   // todo: setup test
-   Credentials testCommentCredentials[] = {
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" },
-           { "", "" }
+   testSet.name = "Comment exploitation";
+   testSet.testsNumber = 5;
+   string errorMessage = "Comments altered how the query should have run";
+   // TODO: setup tests
+   testSet.testCases = new TestCase[5] {
+           // { username, password, expected output }
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage },
+           { Credentials("", ""), "", errorMessage }
    };
 
-   /// test
-   for (auto & credential : testCommentCredentials) {
-      cout << "   Input (username/password):\n"
-           << "      " << credential.username << endl
-           << "      " << credential.password << endl;
-      string query = genQueryFunction(credential.username, credential.password);
+   /// test & validate
+   runTests(testSet);
 
-   /// verify
-      // todo: properly verify
-      if (query == "")
-         cout << "      PASSED: Nothing was injected\n";
-      else
-         cout << "      FAILED: Comments altered how the query should have run\n";
-   }
+   delete testSet.testCases;
+   testSet.testCases = nullptr;
 }
 
 
-//enum TestStatuses {
-//   TODO = 0,
-//   FAILED = 1,
-//   PASSED = 2
-//};
-//
-//struct Test {
-//   string name;
-//   bool (*run)(Credentials*);
-//   TestStatuses status = TODO;
-//};
+ void runTests(TestSet & testSet) {
+    cout << "\t" << testSet.name << ":\n";
 
+    for (int i = 0; i < testSet.testsNumber; i++) {
+       const string username = testSet.testCases[i].input.username;
+       const string password = testSet.testCases[i].input.password;
+       cout << "\t   Input (username/password):\n"
+            << "\t      " << username << endl
+            << "\t      " << password << endl;
 
-// void runTests(string testName, GenQueryFunction genQueryFunction, Credentials* testCredentials, int testsNumber, string errorMessage) {
-//
-//    cout << "\nRunning tests on " << testName << "...\n";
-//
-//    int testsPassedCount = 0;
-//    for (int i = 0; i < testsNumber; i++) {
-//       cout << "   Input (username/password):\n"
-//            << "      " << testCredentials[i].username << endl
-//            << "      " << testCredentials[i].password << endl;
-//       string query = genQueryFunction(testCredentials[i].username, testCredentials[i].password);
-//       cout << "      PASSED: Nothing was injected\n";
-//       cout << "      FAILED: " << errorMessage << endl;
-//    }
-//    for (int i = 0; i < numberOfTests; i++) {
-//       bool testPassed = tests[i].run();
-//       if (testPassed) {
-//          cout << "\t..." << "PASSED\n\n";
-//          tests[i].status = PASSED;
-//          testsPassedCount++;
-//       } else {
-//          cout << "\t...FAILED\n\n";
-//          tests[i].status = FAILED;
-//       }
-//    }
-//
-//    cout << "==========================================================================\n"
-//         << testsPassedCount << "/" << numberOfTests << " tests passed\n";
-// }
+       /// Test
+       const string actualOutput = testSet.functionTested(username, password);
 
+       /// Verify
+       if (actualOutput == testSet.testCases[i].expectedOutput) {
+         testSet.testCases[i].status = PASSED;
+         testSet.testsPassedCount++;
+         cout << "\t      PASSED: Nothing was injected\n";
+       } else {
+         testSet.testCases[i].status = FAILED;
+         cout << "\t      FAILED: " << testSet.testCases[i].errorMessage << endl
+              << "\t         Actual   output: " << actualOutput << endl
+              << "\t         Expected output: " << testSet.testCases[i].expectedOutput << endl;
+       }
+    }
 
-// /**
-//  * Homographs Test 1
-//  * @return
-//  */
-// bool test1() {
-//    // Setup
-//    // Test
-//    // Verify
-//    return false;
-// }
-
-
-
-// /**
-//  * Display a detailed test case report 
-//  */
-// void detailedTestCaseReport(string actualPath1, string inputPath1,
-//                             string actualPath2, string inputPath2) {
-// }
+    /// Total
+    cout << "\t==========================================================================\n"
+         << "\t|| " << testSet.testsPassedCount << "/" << testSet.testsNumber << " tests passed for " << testSet.name << endl
+         << "\t==========================================================================\n\n";
+ }
