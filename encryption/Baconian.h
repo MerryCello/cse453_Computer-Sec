3793f4163b41171064703c2c39132ee303b81622
GrumpyCello <kfoniciello@gmail.com>
2021-07-17 14:55:14 -0600

    changed 1s and 0s string to a char "01010101" -> (char)0b01010101)

--- encryption/Baconian.h
+++ encryption/Baconian.h
@@ -1,10 +1,10 @@
 /********************************************************************
 * Header:
-*    Implement your cipher here. You can view 'example.h' to see the
+*    Implement your cypher here. You can view 'example.h' to see the
 *    completed Caesar Cipher example.
 ********************************************************************/
-#ifndef CIPHER03_H
-#define CIPHER03_H
+#ifndef BACONIAN_H
+#define BACONIAN_H
 
 #include <iostream>
 #include <map>
@@ -22,7 +22,7 @@ public:
 
    /***********************************************************
     * GET CIPHER CITATION
-    * Returns the citation from which we learned about the cipher
+    * Returns the citation from which we learned about the cypher
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
@@ -43,18 +43,18 @@ public:
       // The encryptCase pseudocode
       str =  "encrypt(plainText, password)\n";
       str += "   FOR i is all values of plainText\n";
-      str += "      cipherText += map.find(i)\n";
-      str += "   RETURN cipherText\n\n";
+      str += "      cypherText += map.find(i)\n";
+      str += "   RETURN cypherText\n\n";
 
       // The decryptCase pseudocode
-      str += "decrypt(cipherText, password)\n";
-      str += "   FOR p is all values of cipherText\n";
+      str += "decrypt(cypherText, password)\n";
+      str += "   FOR p is all values of cypherText\n";
       str += "      if string length != 7\n";
-      str += "          string += cipherText[i]\n";
+      str += "          string += cypherText[i]\n";
       str += "      else if string length == 7\n";
       str += "          plainText += map.find(string)\n";
       str += "          clear string\n";
-      str += "          string += cipherText[i]\n";
+      str += "          string += cypherText[i]\n";
       str += "   RETURN plainText\n\n";
 
       return str;
@@ -65,49 +65,31 @@ public:
     * This encrypt() function will be passed a password and
     * some plain text. The plain text will be encrypted using
     * the Baconian Cypher (translating the text into 1's and
-    * 0's) and then returning the cipherText.
+    * 0's) and then returning the cypherText.
     **********************************************************/
    virtual std::string encrypt(const std::string & plainText,
-                               const std::string & password)
-   {
-      std::string cipherText; // Save the text after it has been encrypted
+                               const std::string & password) {
+      std::string cypherText; // Save the text after it has been encrypted
 
       for (int i = 0; i < plainText.length(); i++)
-      {
-         cipherText += cipherMap.find(plainText[i])->second; // Use the map variable that is declared as a private variable
-      }
+         cypherText += cypherMap[plainText[i]]; // Use the map variable that is declared as a private variable
 
-      return cipherText;
+      return cypherText;
    }
 
    /**********************************************************
     * DECRYPT
     * This decrypt() function will be passed a password and
-    * some cipherText. The cipherText will be decrypted using
+    * some cypherText. The cypherText will be decrypted using
     * the Baconian Cypher (translating 1's and 0's into text)
     * in reverse and then returning the plainText.
     **********************************************************/
-   virtual std::string decrypt(const std::string & cipherText,
-                               const std::string & password)
-   {
+   virtual std::string decrypt(const std::string & cypherText,
+                               const std::string & password) {
       std::string plainText; // Save the text after it has been decrypted
-      std::string str; // Save 7 bits of digits every iteration
 
-      for (int i = 0; i < cipherText.length(); i++)
-      {
-         if (str.length() != 7) // As long as str is not 7 bits long, keep iterating
-         {
-            str += cipherText[i];
-         }
-         else if (str.length() == 7) // Once str is 7 bits long, pass it to the reverseMap function
-         {
-            plainText += reverseMap(str);
-            str.clear();
-            str += cipherText[i];
-         }
-      }
-
-      plainText += reverseMap(str);
+      for (const char c : cypherText)
+         plainText += reverseMap(c);
 
       return plainText;
    }
@@ -117,16 +99,11 @@ public:
  * Instead of returning the value using the key in the map,
  * return the key with the value.
  **********************************************************/
-   std::string reverseMap(std::string text) {
-      std::string str;
-
-      for (auto it = cipherMap.begin(); it != cipherMap.end(); ++it) // Do the reverse of what the find() function does
-         {
-            if (it->second == text)
-               str += it->first;
-         }
-
-      return str;
+   char reverseMap(char text) {
+      for (auto & it : cypherMap) // Do the reverse of what the find() function does
+         if (it.second == text)
+            return it.first;
+      return 'd';
    }
 
 private:
@@ -135,106 +112,105 @@ private:
     * Create a map that maps all symbols, digits, and
     * letters to a unique 7-bit binary number.
     *************************************************/
-   std::map<char, std::string> cipherMap = {
-           { 'a', "0000000" },
-           { 'b', "0000001" },
-           { 'c', "0000010" },
-           { 'd', "0000011" },
-           { 'e', "0000100" },
-           { 'f', "0000101" },
-           { 'g', "0000110" },
-           { 'h', "0000111" },
-           { 'i', "0001000" },
-           { 'j', "0001001" },
-           { 'k', "0001010" },
-           { 'l', "0001011" },
-           { 'm', "0001100" },
-           { 'n', "0001101" },
-           { 'o', "0001110" },
-           { 'p', "0001111" },
-           { 'q', "0010000" },
-           { 'r', "0010001" },
-           { 's', "0010010" },
-           { 't', "0010011" },
-           { 'u', "0010100" },
-           { 'v', "0010101" },
-           { 'w', "0010110" },
-           { 'x', "0010111" },
-           { 'y', "0011000" },
-           { 'z', "0011001" },
-           { 'A', "0011010" },
-           { 'B', "0011011" },
-           { 'C', "0011100" },
-           { 'D', "0011101" },
-           { 'E', "0011110" },
-           { 'F', "0011111" },
-           { 'G', "0100000" },
-           { 'H', "0100001" },
-           { 'I', "0100010" },
-           { 'J', "0100011" },
-           { 'K', "0100100" },
-           { 'L', "0100101" },
-           { 'M', "0100110" },
-           { 'N', "0100111" },
-           { 'O', "0101000" },
-           { 'P', "0101001" },
-           { 'Q', "0101010" },
-           { 'R', "0101011" },
-           { 'S', "0101100" },
-           { 'T', "0101101" },
-           { 'U', "0101110" },
-           { 'V', "0101111" },
-           { 'W', "0110000" },
-           { 'X', "0110001" },
-           { 'Y', "0110010" },
-           { 'Z', "0110011" },
-           { '0', "0110100" },
-           { '1', "0110101" },
-           { '2', "0110110" },
-           { '3', "0110111" },
-           { '4', "0111000" },
-           { '5', "0111001" },
-           { '6', "0111010" },
-           { '7', "0111011" },
-           { '8', "0111100" },
-           { '9', "0111101" },
-           { '!', "0111110" },
-           { '@', "0111111" },
-           { '#', "1000000" },
-           { '$', "1000001" },
-           { '%', "1000010" },
-           { '^', "1000011" },
-           { '&', "1000100" },
-           { '*', "1000101" },
-           { '(', "1000110" },
-           { ')', "1000111" },
-           { '-', "1001000" },
-           { '_', "1001001" },
-           { '=', "1001010" },
-           { '+', "1001011" },
-           { '[', "1001100" },
-           { '{', "1001101" },
-           { ']', "1001110" },
-           { '}', "1001111" },
-           { '\\', "1010000" },
-           { '|', "1010001" },
-           { ';', "1010010" },
-           { ':', "1010011" },
-           { '\'', "1010100" },
-           { '"', "1010101" },
-           { '<', "1010110" },
-           { ',', "1010111" },
-           { '>', "1011000" },
-           { '.', "1011001" },
-           { '/', "1011010" },
-           { '?', "1011011" },
-           { '`', "1011100" },
-           { '~', "1011101" },
-           { ' ', "1011110" },
-           { '\n', "1011111" },
-           { '\t', "1100000" }
+   std::map<char, char> cypherMap = {
+           { 'a',  0b10000000 },
+           { 'b',  0b10000001 },
+           { 'c',  0b10000010 },
+           { 'd',  0b10000011 },
+           { 'e',  0b10000100 },
+           { 'f',  0b10000101 },
+           { 'g',  0b10000110 },
+           { 'h',  0b10000111 },
+           { 'i',  0b10001000 },
+           { 'j',  0b10001001 },
+           { 'k',  0b10001010 },
+           { 'l',  0b10001011 },
+           { 'm',  0b10001100 },
+           { 'n',  0b10001101 },
+           { 'o',  0b10001110 },
+           { 'p',  0b10001111 },
+           { 'q',  0b10010000 },
+           { 'r',  0b10010001 },
+           { 's',  0b10010010 },
+           { 't',  0b10010011 },
+           { 'u',  0b10010100 },
+           { 'v',  0b10010101 },
+           { 'w',  0b10010110 },
+           { 'x',  0b10010111 },
+           { 'y',  0b10011000 },
+           { 'z',  0b10011001 },
+           { 'A',  0b10011010 },
+           { 'B',  0b10011011 },
+           { 'C',  0b10011100 },
+           { 'D',  0b10011101 },
+           { 'E',  0b10011110 },
+           { 'F',  0b10011111 },
+           { 'G',  0b10100000 },
+           { 'H',  0b10100001 },
+           { 'I',  0b10100010 },
+           { 'J',  0b10100011 },
+           { 'K',  0b10100100 },
+           { 'L',  0b10100101 },
+           { 'M',  0b10100110 },
+           { 'N',  0b10100111 },
+           { 'O',  0b10101000 },
+           { 'P',  0b10101001 },
+           { 'Q',  0b10101010 },
+           { 'R',  0b10101011 },
+           { 'S',  0b10101100 },
+           { 'T',  0b10101101 },
+           { 'U',  0b10101110 },
+           { 'V',  0b10101111 },
+           { 'W',  0b10110000 },
+           { 'X',  0b10110001 },
+           { 'Y',  0b10110010 },
+           { 'Z',  0b10110011 },
+           { '0',  0b10110100 },
+           { '1',  0b10110101 },
+           { '2',  0b10110110 },
+           { '3',  0b10110111 },
+           { '4',  0b10111000 },
+           { '5',  0b10111001 },
+           { '6',  0b10111010 },
+           { '7',  0b10111011 },
+           { '8',  0b10111100 },
+           { '9',  0b10111101 },
+           { '!',  0b10111110 },
+           { '@',  0b10111111 },
+           { '#',  0b11000000 },
+           { '$',  0b11000001 },
+           { '%',  0b11000010 },
+           { '^',  0b11000011 },
+           { '&',  0b11000100 },
+           { '*',  0b11000101 },
+           { '(',  0b11000110 },
+           { ')',  0b11000111 },
+           { '-',  0b11001000 },
+           { '_',  0b11001001 },
+           { '=',  0b11001010 },
+           { '+',  0b11001011 },
+           { '[',  0b11001100 },
+           { '{',  0b11001101 },
+           { ']',  0b11001110 },
+           { '}',  0b11001111 },
+           { '\\', 0b11010000 },
+           { '|',  0b11010001 },
+           { ';',  0b11010010 },
+           { ':',  0b11010011 },
+           { '\'', 0b11010100 },
+           { '"',  0b11010101 },
+           { '<',  0b11010110 },
+           { ',',  0b11010111 },
+           { '>',  0b11011000 },
+           { '.',  0b11011001 },
+           { '/',  0b11011010 },
+           { '?',  0b11011011 },
+           { '`',  0b11011100 },
+           { '~',  0b11011101 },
+           { ' ',  0b11011110 },
+           { '\n', 0b11011111 },
+           { '\t', 0b11100000 }
    };
-
 };
 
-#endif // CIPHER03_H
+#endif // BACONIAN_H
